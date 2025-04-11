8f57fae8f75a4d78e8bf98b71689bb35c59c0233
Fabiox7778 <fabiorabello08@gmail.com>
2025-04-11 15:45:10 -0300

    Batalha Pokémon: Pikachu vs Charmander

---
+++ Game.js
@@ -0,0 +1,38 @@
+//Definindo rodadas
+let rodadas = 0;
+
+//Definindo vida
+let energiaPikachu = 80;
+let energiaCharmander = 80;
+
+//Definindo ataque
+let ataquePicachu = 0;
+let ataqueCharmander = 0;
+
+//Começar o While
+while (energiaPikachu >= 0 && energiaCharmander >= 0) {
+    //Definindo o numero do dano
+    let ataquePicachu = Math.floor(Math.random() * (25 - 10 + 1) + 10);
+    let ataqueCharmander = Math.floor(Math.random() * (22 - 8 + 1) + 8);
+    //Redefinindo vida
+    energiaPikachu -= ataqueCharmander
+    energiaCharmander -= ataquePicachu
+    //Printando no Terminal
+    console.log("🔁 Rodada", rodadas);
+    console.log(`💥💥 Pikachu usou Thunderbolt e causou ${ataquePicachu} de dano!`);
+    console.log(`💥💥 Charmander usou Burning Flame e causou ${ataqueCharmander} de dano!`);
+    console.log(`⚡ Energia do Charmander: ${energiaCharmander}`);
+    console.log(`🔥 Energia do Pikachu: ${energiaPikachu}`);
+    console.log("--------------------------------------------------------------------------------");
+    rodadas++;
+}
+//Mostrando quantas batalhas foram necessarias
+console.log(`👌A batalha acabou vom ${rodadas} rodadas👌`)
+//Usando if e else para ver quem ganhou e printar no terminal
+if (energiaCharmander > energiaPikachu) {
+    console.log("🏆 Charmander venceu a batalha com um fogo ganhador!")
+} else if (energiaPikachu > energiaCharmander) {
+    console.log("🏆 Pikachu venceu a batalha com um trovão ganhador!")
+} else {
+    console.log("😒Ouve um mortal impate")
+}
